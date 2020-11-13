2804789efcde09a5c6b9659181aa312d5a771b26
Denny Mathew <devdenmathew@gmail.com>
2020-11-13 21:47:29 +0530

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Denny Mathew on 13/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
